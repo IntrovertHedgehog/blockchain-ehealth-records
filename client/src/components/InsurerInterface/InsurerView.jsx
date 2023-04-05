9ed521f8728839ee16ba7de3d27ef2863d8d5039
seancxy <seanchengxy13@gmail.com>
2023-04-05 17:07:28 +0800

    - insurer validate patient claim

--- client/src/components/InsurerInterface/InsurerView.jsx
+++ client/src/components/InsurerInterface/InsurerView.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useState } from "react";
 
 function InsurerView() {
+
+    const [patientAddress, setPatientAddress] = useState("");
+
+    const handlePatientAddressChange = e => {
+        setPatientAddress(e.target.value);
+    };
+
+    const validatePatientClaim = async (e) => {
+        if (e.target.tagName === "INPUT") {
+            return;
+        }
+        if (doctorAddress === "") {
+            alert("Please enter a value to write.");
+            return;
+        }
+        await contract.methods.validateCIClaim(patientAddress).send({ from: accounts[0] });
+    }
+
     return (
         <div className='btns'>
-            <p>Insurer View</p>
+            <div className="input-btn">
+                <input
+                    type="text"
+                    placeholder="Patient Address"
+                    value={patientAddress}
+                    onChange={handlePatientAddressChange}
+                />
+
+                <button onClick={validatePatientClaim}>
+                    Validate Patient CI Claim
+                </button>
+            </div>
 
         </div>
     )
